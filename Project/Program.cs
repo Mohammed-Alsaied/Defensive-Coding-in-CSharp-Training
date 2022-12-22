158e6a05e39554f3b483bae602ae9acb8c5c8cf7
moham <moham@MOHAMED_ALSAEED>
2022-12-22 23:58:30 +0200

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using APM.SL;
+using System;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+            System.Console.WriteLine($" Margin = {product.CalculateMargin(priceInput: "1000", costInput: "500")}");
+
+            var util = Utility.SendEmail(subject: "Day Metting", recipient: "Mohamed", body: "waiting you", sendDate: DateTime.Now, saveCopy: true);
+            System.Console.WriteLine($"Mail Details = {util}");
+        }
+    }
+}
